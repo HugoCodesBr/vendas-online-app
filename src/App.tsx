cdf21e65f6846d1458c54f629beb58f352c25ec8
Hugo Frota <herbtyh@gmail.com>
2023-06-12 21:39:47 -0300

    feat: insert styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
 import { SafeAreaView, Text } from 'react-native';
+import styled from 'styled-components/native';
+
+const TextNew = styled.Text`
+  font-size: 24px;
+  color: pink;
+  background-color: #888;
+  text-decoration: underline;
+  text-transform: uppercase;
+`;
 
 const App = () => {
   return (
     <SafeAreaView>
       <Text>Testando</Text>
+      <TextNew>Novo Teste</TextNew>
     </SafeAreaView>
   );
 };
